d4cdbcf8be604bbd93bb4a97c7d9a2b7875d118d
Rémi Bouillon <rbouillon001@iutbayonne.univ-pau.fr>
2025-12-17 17:16:28 +0100

    Correction Conflit

--- include.php
+++ include.php
@@ -38,7 +38,7 @@ require_once 'controllers/controller_cours.class.php';
 
 require_once 'controllers/controller_classe.class.php';
 
-//require_once 'controllers/controller_joinServer.class.php';
+require_once 'controllers/controller_joinServer.class.php';
 
 require_once 'controllers/controller_devoir.class.php';
 
@@ -46,4 +46,4 @@ require_once 'controllers/controller_factory.php';
 
 require_once 'controllers/controller_register.php';
 
-//require_once 'controllers/controller_forgetpasseword.php';
+require_once 'controllers/controller_forgetpasseword.php';
